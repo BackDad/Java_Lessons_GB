abe618eeefa2687e4dca24e6a6b5e4f519735d28
BackDad <manuk_1122@mail.ru>
2023-03-19 22:08:32 +0300

    Task_1 done

--- src/main/java/Lesson_2/Task_2.java
+++ src/main/java/Lesson_2/Task_2.java
@@ -3,5 +3,44 @@ package Lesson_2;
 public class Task_2 {
     public static void main(String[] args) {
 
+        String s = "aaaaaaabbbbbbcddde";
+        String compressed = compress(s);
+        System.out.println(compressed);
+    }
+    static String compress(String input){
+        String result = "";
+        int count =1;
+        char tmp = input.charAt(0);
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (tmp == c){
+                count++;
+            }
+            else {
+                if (count>1)
+                {
+                    result+=tmp;
+                    result+=count;
+                }
+                else {
+                    result+=tmp;
+
+                }
+                tmp = c;
+                count=1;
+            }
+
+
+        }
+        if (count>1)
+        {
+            result+=tmp;
+            result+=count;
+        }
+        else {
+            result+=tmp;
+
+        }
+     return result;
     }
 }
