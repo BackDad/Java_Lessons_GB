13f54705d7eb244fd3076ba3a6444634a8cb0186
BackDad <manuk_1122@mail.ru>
2023-03-20 15:44:17 +0300

    Task_1 done

--- src/main/java/Task_1.java
+++ src/main/java/Task_1.java
@@ -1,5 +1,20 @@
 public class Task_1 {
     public static void main(String[] args) {
-
+        System.out.println(ANSI_GREEN + buildString(5,'a','b') + ANSI_RESET);
     }
+//    Дано четное число N (>0) и символы c1 и c2.
+//    Написать метод, который вернет строку длины N,
+//    которая состоит из чередующихся символов c1 и c2, начиная с c1. (6, 'a', 'b') -> "ababab"
+    static String buildString(int n, char c1, char c2){
+        String result = "";
+        for (int i = 0; i <n/2+1; i++) {
+            result+=c1;
+            result+=c2;
+        }
+
+        return result;
+    }
+
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
 }
