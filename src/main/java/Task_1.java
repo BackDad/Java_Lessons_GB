8c2a21ea04e82fbd9994f79439e28b070ee00182
BackDad <manuk_1122@mail.ru>
2023-03-20 16:03:20 +0300

    Task_2 done

--- src/main/java/Task_1.java
+++ src/main/java/Task_1.java
@@ -1,6 +1,12 @@
+import java.io.*;
+
 public class Task_1 {
     public static void main(String[] args) {
         System.out.println(ANSI_GREEN + buildString(6,'a','b') + ANSI_RESET);
+
+        TextOut(); //"TEXT x 100"
+
+
     }
 //    Дано четное число N (>0) и символы c1 и c2.
 //    Написать метод, который вернет строку длины N,
@@ -11,10 +17,26 @@ public class Task_1 {
             result+=c1;
             result+=c2;
         }
-
         return result;
     }
+    //Создать файл с названием file.txt. Если файл уже есть, то создавать не надо Записать в него Слово "TEXT" 100 раз
+    static void TextOut(){
+        Writer writer = null;
 
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream("file.txt"), "utf-8"));
+            for (int i = 1; i <=100 ; i++) {
+                writer.write("TEXT "+i);
+                writer.write("\n");
+
+            }
+        } catch (IOException ex) {
+            // Report
+        } finally {
+            try {writer.close();} catch (Exception ex) {/*ignore*/}
+        }
+    }
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RESET = "\u001B[0m";
 }
