dd0fd8f59087c128def8493fd8f863048c40d84e
BackDad <manuk_1122@mail.ru>
2023-03-21 10:41:33 +0300

    Task_1 done

--- src/main/java/Lesson_3/Task_1.java
+++ src/main/java/Lesson_3/Task_1.java
@@ -1,7 +1,29 @@
 package Lesson_3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Task_1 {
     public static void main(String[] args) {
 
+        List<Integer> integers = new ArrayList<>(Arrays.asList(1,1,2,2,3,4,5,6,8,8));
+        System.out.println(integers);
+        removeDuplicates(integers);
+        System.out.println(integers);
     }
-}
+    private static void removeDuplicates(List<Integer> source){
+        int i = 0;
+        while (i<source.size()){
+            int j=i+1;
+            while (j<source.size()){
+                if (source.get(i).equals(source.get(j))){
+                    source.remove(j);
+                }
+                j++;
+            }
+            i++;
+        }
+            
+        }
+    }
