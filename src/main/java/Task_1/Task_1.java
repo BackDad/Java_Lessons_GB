5d18285ae5e2eb3886b00f94ffb89b54adf085b1
BackDad <manuk_1122@mail.ru>
2023-03-23 07:42:42 +0300

    применение переопределенного метода toString в классе Point2D по умолчанию для a

--- src/main/java/Task_1/Task_1.java
+++ src/main/java/Task_1/Task_1.java
@@ -11,7 +11,7 @@ public class Task_1 {
         System.out.println(a); //System.out.println(a) и System.out.println(a.toString()); одно и тоже вернет ссылку
         Point2D b = new Point2D(2,50);
 
-        System.out.println(b.getInfo());//печать информации через метод getInfo описанный в классе Point2D
+        System.out.println(b);//печать информации через метод getInfo описанный в классе Point2D
         System.out.println(OOPVectorLen(a,b));
 
         Point2D a1 = new Point2D(10);
