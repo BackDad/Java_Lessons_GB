b930db9614649eb1b5394a184fde442856904467
BackDad <manuk_1122@mail.ru>
2023-03-23 07:20:20 +0300

    создание экземпляра класса и вызов конструктора Point2D cо значением Value

--- src/main/java/Task_1/Task_1.java
+++ src/main/java/Task_1/Task_1.java
@@ -14,6 +14,12 @@ public class Task_1 {
         System.out.println(b.toString());
         System.out.println(OOPVectorLen(a,b));
 
+        Point2D a1 = new Point2D(10);
+        Point2D b1 = new Point2D(10);
+        System.out.println(OOPVectorLen(a1,b1));
+
+
+
     }
     public static double OOPVectorLen(Point2D a, Point2D b){
         return Math.sqrt(Math.pow((a.x-b.x),2)+Math.pow((a.y-b.y),2));
