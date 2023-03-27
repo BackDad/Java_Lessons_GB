b0f39d1395327aa06fbfd7d671dab5e30e8b011c
BackDad <manuk_1122@mail.ru>
2023-03-27 19:07:48 +0300

    Start new Homework

---
+++ src/main/java/Home_work_4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Home_work_4 {
+    public static void main(String[] args) {
+        String ss = "sas";
+        String s = "";
+        ArrayList <String> data = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        while (!s.equals("exit")) {
+            s = sc.nextLine();
+
+                    }
+        System.out.println("Гудбай");
+
+
+    }
+    public static void addData (String data , Integer position){
+
+    }
+    public static void printData(ArrayList<String> data, Integer position){
+        System.out.println(data[position]);
+    }
+
+}
